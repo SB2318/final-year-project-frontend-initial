2c321e38299f76655fb35aa6ea2258ed140b0733
SB2318 <susmitabhattacharya2023@gmail.com>
2023-03-05 00:29:10 +0530

    Card Work starting

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,62 @@
+import React,{useState} from 'react';
+import "../Styles/Navigation.css";
+import { Link } from 'react-router-dom';
+import {FaBars,FaTimes} from "react-icons/fa";
+
+
+const Navigation = () => {
+
+  const [navClick, setNavClick] = useState(false);
+  const[bgColor,setBgColor] = useState(false);
+
+  const handleClick= ()=>{
+    setNavClick(!navClick);
+  };
+
+  const changeColor =() =>{
+
+    // If our website scrolled a fixed amount of pixel,then background changed
+    if(window.scrollY >= 100){
+      setBgColor(true);
+    }else{
+      setBgColor(false);
+    }
+  };
+
+  window.addEventListener("scroll",changeColor);
+
+  return (
+    <div className={ bgColor? "header header-bg" : "header" }>
+         <Link to="/">
+           
+          <img src="https://media.licdn.com/dms/image/C510BAQE5whJbJz5oGg/company-logo_200_200/0/1585130580548?e=2147483647&v=beta&t=e5gJElC3yCvJHTVAqnoa80I9f2tc528sqtjyP_abZ-Y"
+                 height="50" width="50" style={{marginLeft:9,marginTop:14}}/>
+            
+            <h3>Recipes</h3>
+
+        </Link>
+        <ul className={navClick ? "nav-menu active":"nav-menu"}>
+         <li>
+          <Link to="/">Home</Link>
+         </li>
+         <li>
+          <Link to="/about">About</Link>
+         </li>
+        
+         <li>
+          <Link to="/contact">Contact</Link>
+         </li>
+        </ul>
+
+  <div className="hamburger" onClick={handleClick}>
+
+     {navClick ? 
+      (<FaTimes size={20} style={{color:"#fff"}}/>
+       ): (<FaBars size={20} style={{color:"#fff"}}/>)}
+
+   </div>
+    </div>
+  )
+}
+
+export default Navigation
