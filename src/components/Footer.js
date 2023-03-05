e84871e4f0f2962793eaa14e4c4c6a17494ec5d9
SB2318 <susmitabhattacharya2023@gmail.com>
2023-03-05 18:32:37 +0530

    Footer Done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,18 +12,19 @@ const Footer = () => {
         <div className="right">
           <h4>A recipe website made by Charlie's Angels</h4>
           <p>This website is our final year project. </p>
-
+          <p>© Copyright Team <b>Charlie's Angel</b> All Rights Reserved</p>
+          <p>Designed by <b>Susmita Bhattacharya</b></p>
           <div className="social-link">
 
-          <a href="https://www.linkedin.com/in/s-bhattacharya-3687621b7/" target="_blank">
+          <a href="https://www.linkedin.com/school/jalpaiguri-government-engineering-college/?originalSubdomain=in" target="_blank">
           <FaLinkedin size={25} style={{color: "#fff", marginRight:"1rem"}}/>
           </a>
 
-          <a href="https://mobile.twitter.com/SUSMITA2360" target="_blank">
+          <a href="https://twitter.com/jgec_India" target="_blank">
           <FaTwitter size={25} style={{color: "#fff", marginRight:"1rem"}}/>
           </a>
           
-          <a href="https://github.com/SB2318" target="_blank">
+          <a href="https://github.com/SB2318/recipe-website" target="_blank">
           <FaGithub size={25} style={{color: "#fff", marginRight:"1rem"}}/>
           </a>
 
