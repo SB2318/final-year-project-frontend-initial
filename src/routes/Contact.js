324d835f7e8c4425bd889af4c2a49e2da3782fbb
KUMARIPRAGYA <pragyasharma5111999@gmail.com>
2023-04-25 01:41:36 +0530

    SEO Initial done

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,10 +2,22 @@ import React from 'react'
 import Navigation from "../components/navigation/Navigation"
 import ContactContent from "../components/ContactContent"
 import Footer from "../components/Footer"
-
+import { Helmet } from "react-helmet";
 const Contact = () => {
   return (
     <div>
+         <Helmet>
+  <title>Get in touch:Reach out to us</title>
+  
+  <meta>
+  name="description"
+  content="Get in touch:Reach out to us."
+  </meta>
+  <meta>
+  name="keywords"
+  content="contact us"
+  </meta>
+</Helmet>
         <Navigation/>  
         
         <ContactContent/>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,10 +3,23 @@ import Navigation from "../components/navigation/Navigation"
 import HomeContent from "../components/home/HomeContent"
 import RecipeCard from "../components/home/RecipeCard"
 import Footer from "../components/Footer"
-
+import { Helmet } from "react-helmet";
 const Home = () => {
   return (
     <div>
+      <Helmet>
+  <title>Cooking with love:Comforting recipes for every season.</title>
+  
+  <meta>
+  name="description"
+  content="Indulge in the delicious and wholesome world of home cooking with our mouth-watering recipes. Join us on a journey of culinary creativity."
+  </meta>
+  <meta>
+  name="keywords"
+  content="Good food at home,Best food recipe,What should i cook for dinner,Easiest recipe for students,
+  10-minute recipes"
+  </meta>
+</Helmet>
      <Navigation/>
      <HomeContent/>
      <RecipeCard
